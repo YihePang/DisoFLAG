712aacb0ade3b0c1b5bed4e1585308206e8167c6
Yihe <18811020896@163.com>
2023-11-10 11:57:59 +0800

    delete

--- protTrans/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Yihe Pang
-# @Date:   2022-12-25 09:57:09
-# @Last Modified by:   Yihe Pang
-# @Last Modified time: 2022-12-25 12:03:17
-import sys # 即添加包名的搜索路径
-sys.path.append("protTrans")
